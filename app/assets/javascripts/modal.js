62d061fba4fd1b11f270870d8cca37825d0cc2c3
Blanca Sanchez <blancarocio.sanchez@gmail.com>
2016-07-27 23:40:19 +0200

    Añadidas ventanas modales

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,30 @@
+function myFunctionJS(){	
+	// Get the modal
+	var modal = document.getElementById('myModal1');
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("btnMyModal");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	var btnClose = document.getElementById("btnCloseModal");
+
+	// When the user clicks on the button, open the modal 
+	btn.onclick = function() {
+	    modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	btnClose.onclick = function(){
+		displayModal(modal, "none");
+	}
+}
+
+function displayModal(wndModal, strDisplay){
+	wndModal.style.display = strDisplay;
+}
